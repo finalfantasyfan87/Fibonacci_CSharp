22eda7ef110f2943759c431de0c0ac2fa50e39de
Jenster <finalfantasyguru8705@gmail.com>
2019-09-23 19:23:15 -0500

    Update Fibonacci.cs

--- HW/Fibonacci.cs
+++ HW/Fibonacci.cs
@@ -8,10 +8,14 @@ namespace Homework1_jhaywoo2
         //f(n) = f(n - 1) + f(n - 2);
         public int GetFibonacciSeries(int i)
         {
+            //wanted to use recursion versus iteration
+            //also wanted to use a switch statement versus if else
             switch (i)
             {
+                    //base case
                 case 0:
                     return 0;
+            //base case
                 case 1:
                     return 1;
                 default:
